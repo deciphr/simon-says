3a088d51bd28b8a45eff3b8ce8879b3994604788
deciphr <destinylegend132@gmail.com>
2022-07-13 15:49:00 -0500

    added client, tweaked server

---
+++ src/client.py
@@ -0,0 +1,23 @@
+"""
+File: client.py
+Desc: Client script for Simon Says game
+Author: deciphr
+Date: 07/13/22
+"""
+
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '127.0.0.1' #'10.190.23.40'
+port = 42069
+
+client.connect((host, port))
+print(f"Connecting to {host}:{port}")
+
+try:   
+    while True:
+        msg = input("Message: ")
+        client.send(msg.encode())
+except KeyboardInterrupt:
+    print("\nClosing!")
--- src/server.py
+++ src/server.py
@@ -5,37 +5,46 @@ Author: deciphr
 Date: 07/13/22
 """
 
-from re import X
 import netifaces as ni
 import socket
 import threading
 DEBUG = False
 
-def on_client_connect(client_socket, addr):
-    while True:
-        msg = client_socket.recv(1024)
-        print(f"{addr}: msg")
+def on_client_connect(client, addr):
+    print(f"Connect: {addr}")
 
-    client_socket.close()
+    try:
+        while True:
+            msg = client.recv(1024)
+            print(f"{addr}: {msg.decode()}")
+
+            client.send("Receieved!".encode())
+    finally:
+        client.close()
+        print(f"Disconnected: {addr}")
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
     port = 42069
 
-    server.bind('127.0.0.1', port)
+    server.bind(('127.0.0.1', port))
     if DEBUG: print("Server binded")
 
     server.listen(5)
     print(f"Server started: {host}:{port}")
     print("Listening for clients")
 
-    try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind(('127.0.0.1', port))
+        server.listen(1)
+
         while True:
-            client, addr = server.accept()
-            threading.Thread(target=on_client_connect, args=(client, addr))
-    except KeyboardInterrupt:
-        print("\nClosing!")
+            conn, addr = server.accept()
+            with conn:
+                client_thread = threading.Thread(target=on_client_connect, args=(conn, addr))
+                client_thread.start()
+                client_thread.join()
 
 if __name__ == "__main__":
     main()
